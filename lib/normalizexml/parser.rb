990eda72fdbaa7dbdaa7263edf24383430c11af1
Jeff McAffee <jeff@ktechsystems.com>
2011-09-02 16:55:00 +0000

    Completing change-over to Nokogiri. All normalization is now performed by the Nokogiri library.

--- lib/normalizexml/parser.rb
+++ lib/normalizexml/parser.rb
@@ -44,34 +44,87 @@ module NormalizeXml
 			
 			raise ArgumentError.new("infile not provided") unless !@infile.nil? && !@infile.empty?
 			if( @outfile.nil? || @outfile.empty? )
-				@outfile = File.dirname(@infile) + File.basename(@infile, ".xml") + ".nml.xml"
+				newfilename = File.basename(@infile, ".xml") + ".nml.xml"
+				@outfile = File.join( File.absolute_path( File.dirname(@infile) ), newfilename )
 			end
 			
-			# Open the file and parse each line...
-			File.open(@infile, "r") do |f|
-				File.open(@outfile, "w") do |fout|
-					f.each do |lineIn|
-						fout << parseLine(lineIn)
-					end
-				end
-			end
+			puts "Normalizing file: #{@infile}"
+			puts
 			
-			f = File.open(@outfile, 'r')
+			f = File.open(@infile, 'r')
 			doc = Nokogiri::XML(f)
 			f.close
+			
+			normalize_ids(doc)
+			normalize_orders(doc)
+			normalize_ppm_datatypes(doc)
 			
 			f = File.open(@outfile, 'w')
 			doc.write_xml_to(f)
 			f.close
 			
-			puts "Normalization complete."
-			puts
-			puts "Input file:  #{@infile}"
+			puts "Normalization of #{@infile} complete."
 			puts "Output file: #{@outfile}"
 			puts
 		end
 		  
 	  
+		###
+		# Normalize Id attributes to 0
+		# doc:: Nokogiri::XML document
+		#
+		def normalize_ids(doc)
+			# Normalize all ruleset Ids
+			rulesets = doc.xpath('//Ruleset')
+			rulesets.each do |rs|
+				rs['Id'] = "0"
+			end
+			
+			# Normalize all rule Ids
+			rules = doc.xpath('//Rule')
+			rules.each do |r|
+				r['Id'] = "0"
+			end
+			
+		end
+		
+		
+		###
+		# Normalize Order attributes to 0
+		# doc:: Nokogiri::XML document
+		#
+		def normalize_orders(doc)
+			# Normalize all Compute Order attributes by removing them
+			nodes = doc.xpath('//Compute')
+			nodes.each do |n|
+				n.remove_attribute('Order')
+			end
+			
+			# Normalize all AssignTo Order attributes by removing them
+			nodes = doc.xpath('//AssignTo')
+			nodes.each do |n|
+				n.remove_attribute('Order')
+			end
+			
+		end
+		
+		
+		###
+		# Normalize PPM DataType attributes by removing them
+		# doc:: Nokogiri::XML document
+		#
+		def normalize_ppm_datatypes(doc)
+			# Normalize all PPM DataType attributes by removing them
+			nodes = doc.xpath('//PPM')
+			nodes.each do |n|
+				n.remove_attribute('DataType')
+			end
+			
+		end
+		
+		
+		###
+		# DEPRECATED - DO NOT USE
 		def parseLine(ln)
 			out = ln.gsub( /Order=\"(\d+)\"/, 'Order="0"')
 			out = out.gsub( /Order= '(\d+)'/, 'Order="0"')
