f8af5f0e218a2e55ee2d2dd69f4c0155db3a4ac2
Jeff McAffee <jeff@ktechsystems.com>
2014-06-04 15:18:42 -0700

    Version 0.1.2

--- lib/normalizexml/version.rb
+++ lib/normalizexml/version.rb
@@ -3,9 +3,9 @@
 #
 module NormalizeXml
 
-  VERSION = "0.1.1" unless constants.include?("VERSION")
+  VERSION = "0.1.2" unless constants.include?("VERSION")
   APPNAME = "NormalizeXml" unless constants.include?("APPNAME")
-  COPYRIGHT = "Copyright (c) 2013, kTech Systems LLC. All rights reserved." unless constants.include?("COPYRIGHT")
+  COPYRIGHT = "Copyright (c) 2014, kTech Systems LLC. All rights reserved." unless constants.include?("COPYRIGHT")
 
 
   def self.logo()
