445f8b35f772fe7df0bfed8a862df4949b1cfd81
Jeff McAffee <jeff@ktechsystems.com>
2015-08-14 10:04:57 -0700

    Bump version to 0.1.3

--- lib/normalizexml/version.rb
+++ lib/normalizexml/version.rb
@@ -3,7 +3,7 @@
 #
 module NormalizeXml
 
-  VERSION = "0.1.2" unless constants.include?("VERSION")
+  VERSION = "0.1.3" unless constants.include?("VERSION")
   APPNAME = "NormalizeXml" unless constants.include?("APPNAME")
   COPYRIGHT = "Copyright (c) 2014, kTech Systems LLC. All rights reserved." unless constants.include?("COPYRIGHT")
 
